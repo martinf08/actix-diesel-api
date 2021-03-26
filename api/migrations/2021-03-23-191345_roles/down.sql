dc026477a653e5e09100c00afb587001534b06b5
Martin <martin.fontana08@gmail.com>
2021-03-26 21:10:38 +0100

    Remove hack

--- api/migrations/2021-03-23-191345_roles/down.sql
+++ api/migrations/2021-03-23-191345_roles/down.sql
@@ -1,7 +1,5 @@
 -- This file should undo anything in `up.sql`
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS roles;
 ALTER TABLE users
     DROP FOREIGN KEY users_ibfk_1,
     DROP role_id;
-SET FOREIGN_KEY_CHECKS=1;
+DROP TABLE IF EXISTS roles;
